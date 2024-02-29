8a2cdc44ca95923cb83621885fce9125406e4c16
DIEGO_CORREA_MONTES <a.21308051280347@cbtis128.edu.mx>
2024-02-29 18:54:48 +0000

    actualizado

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,87 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(AppMiTabBar());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class AppMiTabBar extends StatelessWidget {
+  const AppMiTabBar({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
+      debugShowCheckedModeBanner: false,
+      title: "Ejemplo Tabbar Diego Correa",
       theme: ThemeData(
-        // useMaterial3: true,
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey,
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MiPaginaInicial(),
     );
   }
-}
+} //fin AppMiTabBar
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+//stateful
+class MiPaginaInicial extends StatefulWidget {
+  const MiPaginaInicial({Key? key}) : super(key: key);
 
   @override
+  State<MiPaginaInicial> createState() => _MiPaginaInicialState();
+} //MiPaginaInicial
+
+class _MiPaginaInicialState extends State<MiPaginaInicial> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
-    );
-  }
-}
+    return DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Tabbar Diego Correa"),
+            centerTitle: true,
+            bottom: TabBar(tabs: [
+              Tab(
+                text: "Nissan",
+                icon: Icon(Icons.account_circle),
+              ),
+              Tab(
+                text: "Mazda",
+                icon: Icon(Icons.add_reaction),
+              ),
+              Tab(
+                text: "Honda",
+                icon: Icon(Icons.accessibility_outlined),
+              ),
+              Tab(
+                text: "Toyota",
+                icon: Icon(Icons.airport_shuttle_sharp),
+              ),
+            ]), // fin bottom tabbar
+          ),
+          body: TabBarView(children: const <Widget>[
+            Center(
+              child: Text(
+                "Autos Nissan",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+            ),
+            Center(
+              child: Text(
+                "Autos Mazda",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+            ),
+            Center(
+              child: Text(
+                "Autos Honda",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+            ),
+            Center(
+              child: Text(
+                "Autos Toyota",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+            ),
+          ] //jardin de niños
+              ), // fin de tabbarview
+        ) //fin de scaffold
+        ); //DefaultTabController
+  } // fin widget
+} //_MiPaginaInicialState
